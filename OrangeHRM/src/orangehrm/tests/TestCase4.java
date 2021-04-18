fee7d994020c1be867234ff511c1885f5ba9354e
naveenpalugulla <naveenipe09@gmail.com>
2021-04-19 00:41:02 +0530

    4th testcase -Modified 4th test case

---
+++ OrangeHRM/src/orangehrm/tests/TestCase4.java
@@ -0,0 +1,11 @@
+package orangehrm.tests;
+
+public class TestCase4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testcase 4 updated");
+
+	}
+
+}
